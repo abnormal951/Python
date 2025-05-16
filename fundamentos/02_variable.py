fb27c93af04ae4828ef5cae235c9cafd6cef89f3
Nab San <abnormal951@gmail.com>
2025-05-16 12:48:07 -0600

    variables y operadores logicos

---
+++ fundamentos/02_variable.py
@@ -0,0 +1,9 @@
+nombre='Nabor'
+edad=30
+estatura=1.61
+boolean_positivo=True
+boolean_falso=False
+
+print(type(boolean_positivo))
+print(type(estatura))
+print(type(nombre))
---
+++ fundamentos/operadores_matematicos.py
@@ -0,0 +1,28 @@
+suma =1+2
+resta=2-3
+multiplicacion=2*3
+division=5/9 #siempre da float en el tipo de dato final, no importa que sean 2 enteros
+residuo=9%2 #nos da el residuo de nueve dividido en 2 
+exponente= 3**3
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(residuo)
+print(exponente)
+print(type(division))
+
+#operaciones con strings
+texto='hola'
+texto=texto*3
+print(texto)
+
+#operadores comparativos o relacionales
+print(1>2)
+print(1==2)
+print(1!=1)
+print("texto" == "texto")
+print("texto" != "texto")
+
+
