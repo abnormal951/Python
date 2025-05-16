fb53a0bcb22bcaeba122e408c712cfb25cf9c5f7
Nab San <abnormal951@gmail.com>
2025-05-16 13:03:55 -0600

    buit-in functions

---
+++ fundamentos/04_built_in_functions.py
@@ -0,0 +1,6 @@
+#las built-in functions ya vienes construidas y definidas desde que instalamos python
+#ejemplos print,int,float,str,type
+print(type('text'))
+print(type(int(3.9999)))
+print(type(3.9999))
+#help() #nos ayuda con la documentacion una funcion
