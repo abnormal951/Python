799417a9593b765079bb896947dbdab7d627bb35
Nabor <abnormal951@gmail.com>
2025-05-19 11:45:23 -0600

    Notas iniciales

---
+++ Estructuras de datos/01_listas.py
@@ -0,0 +1,15 @@
+# son las estructuras de datos nos permiten guardar otros datos de manera ordenada
+# permiten manipulacion, extraccion, busqueda e insercion
+# Son eficientes
+# Son mutables si sus elementos pueden sen modificados despues de ser creados
+'''Existen 4 tipos de estrucruras de datos:
+* Listas
+* Tuplas
+* Diccionarios 
+* Sets
+'''
+
+#Listas: permiten guardar elementos de manera ordenada, son odenadas
+
+lenguajes =['Python','Java','Go lang']
+print(lenguajes)
