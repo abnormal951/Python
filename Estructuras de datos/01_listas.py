e0833ebc978bce382a57cf598d9f89c6d07c3610
Nabor <abnormal951@gmail.com>
2025-05-19 20:27:30 -0600

    Se agrean ejercicos de listas, tuplas, diccionarios y sets

--- Estructuras de datos/01_listas.py
+++ Estructuras de datos/01_listas.py
@@ -5,11 +5,56 @@
 '''Existen 4 tipos de estrucruras de datos:
 * Listas
 * Tuplas
-* Diccionarios 
+* Diccionarios
 * Sets
 '''
 
-#Listas: permiten guardar elementos de manera ordenada, son odenadas
+#Listas: permiten guardar elementos de manera ordenada, son odenadas,
+# son mutables, reciben elementos repetidos, se declara con []
 
-lenguajes =['Python','Java','Go lang']
-print(lenguajes)
+print('Listas Ejercicio 1:')
+lista = [1,2,3,4,5,6,7,8,9,10]
+print(lista[0])
+print(lista[9])
+#lista.insert(5,5)
+lista[2] =100
+lista.remove(4)
+lista.append(20)
+print(lista)
+
+print('Listas Ejercicio 2:')
+numeros = [3, 8, 12, 5, 9, 10]
+lista_pares=[num for num in numeros if num % 2==0]
+print(lista_pares)
+print(type(lista_pares))
+
+#Tuplas: no son mutables, se declara con ()
+print('Tuplas Ejercicio 1:')
+tupla = ('lunes','martes','miercoles','jueves','viernes','sabado','domingo')
+print(type(tupla))
+tupla[2]
+tupla=list(tupla)
+print(type(tupla))
+tupla[2]='miércoles'
+tupla=tuple(tupla)
+print(type(tupla))
+print()
+
+#Diccionarios se declaran con {} y son por acceden a ellos por clave valor
+
+print('Diccionario Ejercicio 1:')
+diccionario = {"Juan": 25, "Ana": 30}
+print(diccionario)
+print(diccionario['Ana'])
+diccionario.update({"Luis": 40})
+print(diccionario)
+del diccionario['Juan']
+print(diccionario)
+
+#set se definen con {} y son como las listas.
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+set3=set1.intersection(set2)
+print(set3)
